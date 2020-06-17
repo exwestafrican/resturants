22ac58bd820b0afc4c8515e14061447f0e5faaf3
Tumise <oladele.alade@gmail.com>
2020-06-17 20:38:26 +0100

    resturant settings

--- resturants/settings.py
+++ resturants/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT  = os.path.join(BASE_DIR,"media-root")
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
