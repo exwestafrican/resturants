82c29f23ff21337a241b983d71381448ad51d25e
Tumise <oladele.alade@gmail.com>
2020-06-17 20:36:29 +0100

    static files

--- resturants/settings.py
+++ resturants/settings.py
@@ -136,15 +136,15 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
 MEDIA_ROOT  = os.path.join(BASE_DIR,"media-root")
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 AUTH_USER_MODEL = 'accounts.User'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 #filtering set up
 REST_FRAMEWORK = {
