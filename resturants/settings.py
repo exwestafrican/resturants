b64d94df449659d9bafd258c08e3a6aeee513d87
Tumise <oladele.alade@gmail.com>
2020-06-17 19:56:15 +0100

    secuity updates

--- resturants/settings.py
+++ resturants/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-^m*kkm*2z_b#y1a3dck7(brvxydy3lz$nk9^u&=woly)-3#ni'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['surce-delight.herokuapp.com']
 
 
 # Application definition
