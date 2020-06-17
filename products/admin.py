55f164dba407db6879109f2bc59ff663e103665d
Tumise <oladele.alade@gmail.com>
2020-06-17 21:20:39 +0100

    admin pages update

--- products/admin.py
+++ products/admin.py
@@ -6,8 +6,19 @@ from products.models import (Product,
 
 # Register your models here.
 
+class CategoryAdmin(admin.ModelAdmin):
+    readonly_fields = ['slug']
 
-admin.site.register(Product)
-admin.site.register(ProductVariation)
+class ProductAdmin(admin.ModelAdmin):
+    readonly_fields = ['slug']
+
+class ProductVariationAdmin(admin.ModelAdmin):
+    readonly_fields = ['slug']
+
+
+
+
+admin.site.register(Product,ProductAdmin)
+admin.site.register(ProductVariation,ProductVariationAdmin)
 admin.site.register(ProductImage)
-admin.site.register(Category)
+admin.site.register(Category,CategoryAdmin)
